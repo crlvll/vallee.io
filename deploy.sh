c860474a4b4981b1d69e52a01f98b3402b930b2e
crlvll <rocinante@protonmail.com>
2024-08-28 22:00:43 +0200

    Corrected deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-hugo -d htdocs && ssh cyril@arveed.com "doas chown -R cyril:cyril /var/www/htdocs" && rsync -avz --delete htdocs cyril@arveed.com:/var/www/ && ssh cyril@arveed.com "doas chown -R www:www /var/www/htdocs"
+hugo -d htdocs && ssh cyril@vallee.io "doas chown -R cyril:cyril /var/www/htdocs" && rsync -avz --delete htdocs cyril@vallee.io:/var/www/ && ssh cyril@vallee.io "doas chown -R www:www /var/www/htdocs"
 echo "All synced! Have a nice day."
+sleep 3
+open -a safari https://vallee.io
 exit 0
